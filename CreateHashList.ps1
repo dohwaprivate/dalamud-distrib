df64ea7a83fd9ccd364d9df2dc5e1088c408d7ee
DoHwa <kangdohwa0310@gmail.com>
2024-02-20 18:14:45 +0900

    Initial

---
+++ CreateHashList.ps1
@@ -0,0 +1,15 @@
+$hashes = [ordered]@{}
+
+Set-Location $args[0]
+
+Get-ChildItem -File -Recurse -Exclude dalamud.txt, *.zip, *.pdb, *.ipdb | Foreach-Object {
+	$key = ($_.FullName | Resolve-Path -Relative).TrimStart(".\\")
+	$val = (Get-FileHash $_.FullName -Algorithm MD5).Hash
+	$hashes.Add($key, $val)
+}
+
+$hashes | ConvertTo-Json | Out-File -FilePath "hashes.json"
+
+$mainhash = (Get-FileHash "hashes.json" -Algorithm MD5).Hash
+Set-Location ..
+return $mainhash
